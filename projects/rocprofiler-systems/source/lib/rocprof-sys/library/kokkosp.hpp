e88b67d6609fb68b3a586d6c5f9976a49727db83
Milan Radosavljevic <milan.radosavljevic@amd.com>
2026-03-05 13:30:43 +0100

    [rocprofiler-systems] Add pause/resume for kokkosp component (#3782)
    * Add pause/resume for kokkosp component
    
    * Applied suggestions from code review

---
+++ projects/rocprofiler-systems/source/lib/rocprof-sys/library/kokkosp.hpp
@@ -0,0 +1,20 @@
+// Copyright (c) Advanced Micro Devices, Inc.
+// SPDX-License-Identifier: MIT
+
+#pragma once
+
+#include <vector>
+
+namespace rocprofsys
+{
+namespace kokkosp
+{
+
+void
+pause();
+
+void
+resume();
+
+}  // namespace kokkosp
+}  // namespace rocprofsys
