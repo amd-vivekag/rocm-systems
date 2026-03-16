82e3027686bd52182db58208c92617e0820dbff9
Marko Arandjelovic <Marko.Arandjelovic@amd.com>
2026-03-16 13:29:07 +0000

    Fix build issue

--- projects/clr/rocclr/device/rocm/rocvirtual.cpp
+++ projects/clr/rocclr/device/rocm/rocvirtual.cpp
@@ -2552,7 +2552,7 @@ void VirtualGPU::submitSvmPrefetchAsync(amd::SvmPrefetchAsyncCommand& cmd) {
 
 // ================================================================================================
 void VirtualGPU::SubmitSvmPrefetchBatchAsync(amd::SvmPrefetchBatchAsyncCommand& command) {
-  amd::ScopedLock lock(execution());
+  std::scoped_lock lock(execution());
   profilingBegin(command);
 
   auto wait_events = Barriers().WaitingSignal(HwQueueEngine::Unknown);
