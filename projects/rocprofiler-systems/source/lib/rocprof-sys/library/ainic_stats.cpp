b8cd2eba72cffbab9a2e7947e01ba7a95391c27d
Milan Radosavljevic <milan.radosavljevic@amd.com>
2026-03-20 13:13:50 +0100

    Fix shadowing errors

--- projects/rocprofiler-systems/source/lib/rocprof-sys/library/ainic_stats.cpp
+++ projects/rocprofiler-systems/source/lib/rocprof-sys/library/ainic_stats.cpp
@@ -94,13 +94,12 @@ ai_nic_stats_collector::update_stats()
         }
         for(uint32_t idx = 0; idx < processor_count; ++idx)
         {
-            amdsmi_status_t                status;
-            amdsmi_nic_rdma_devices_info_t info;
-            status = amdsmi_get_nic_rdma_dev_info(processor_handles[idx], &info);
-            if(status != AMDSMI_STATUS_SUCCESS) continue;
+            auto            info = std::make_unique<amdsmi_nic_rdma_devices_info_t>();
+            amdsmi_status_t status2 =
+                amdsmi_get_nic_rdma_dev_info(processor_handles[idx], info.get());
+            if(status2 != AMDSMI_STATUS_SUCCESS) continue;
 
-            // Update info and stats.
-            update_data_for_one_handle(processor_handles[idx], info);
+            update_data_for_one_handle(processor_handles[idx], *info);
         }
     }
 #endif  // AINIC_SUPPORTED
